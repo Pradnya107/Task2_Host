3752dc2426b3aaf0a34751eb0858fc662b3e4e1c
Pradnya Suryawanshi <pradnya.suryawanshi17@siesgst.ac.in>
2019-08-01 22:43:23 +0530

    Hosted

---
+++ precache-manifest.6d6dbc1f08b4e0bafaed66ac8937b3f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537296b1d12be2735f7bbbb0ee663434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "090b22e3946824b92982",
+    "url": "/static/css/main.c9aa5c69.chunk.css"
+  },
+  {
+    "revision": "fb91f2193b7fbe722309",
+    "url": "/static/js/2.deb42900.chunk.js"
+  },
+  {
+    "revision": "090b22e3946824b92982",
+    "url": "/static/js/main.320eab69.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
